9d939bc2f163f572ca756375e2f7a683576221eb
Jessica Villa <suhkha92@gmail.com>
2023-06-01 14:12:39 -0600

    display chats of the multiple topics

--- resources/js/Pages/Chat/main.vue
+++ resources/js/Pages/Chat/main.vue
@@ -2,7 +2,8 @@
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Chat
+                <chat-select v-if="currentChat.id" :chats="chats" :currentChat="currentChat"
+                v-on:chatChanged="setChat($event)"/>
             </h2>
         </template>
 
@@ -21,12 +22,14 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import MessageContainer from './messageContainer.vue';
 import InputMessage from './input.vue';
+import ChatSelect from './chatSelect.vue'
 
 export default {
     components: {
         AppLayout,
         MessageContainer,
-        InputMessage
+        InputMessage,
+        ChatSelect
     },
     data: function() {
         return {
